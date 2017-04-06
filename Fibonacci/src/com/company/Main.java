022039b8f8aa161b9121f7c4544ac88310a1fd40
Novika Dian Renanda <ndrenanda22@gmail.com>
2017-04-06 09:41:53 +0700

    Update Main.java

--- Fibonacci/src/com/company/Main.java
+++ Fibonacci/src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
         fib[1] = 1;
          
         for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
+            fib[i] =fib[i-2]+ fib[i-1]  ;
         }
          
         for (int i = 0; i < n; i++) {
