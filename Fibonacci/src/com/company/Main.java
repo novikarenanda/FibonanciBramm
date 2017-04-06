9f73d8c5cddaee7e14d333ea1a129f1d47e63297
Novika Dian Renanda <ndrenanda22@gmail.com>
2017-04-06 09:39:45 +0700

    Update Main.java

--- Fibonacci/src/com/company/Main.java
+++ Fibonacci/src/com/company/Main.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan Angka : ");
-        int angka = sc.nextInt();
-        int x = 0, y = 1;
-        System.out.println("Hasil : ");
-        for (int i = 0; i < angka+1; i++) {
-            if(i != 0) {
-                System.out.print(x + " ");
-            }
-            x = x + y;
-            y = x - y;
+        Scanner show = new Scanner(System.in);
+        System.out.print("Input :  ");
+        int n = show.nextInt();
+        long fib[] = new long[n];
+         
+        fib[0] = 0;
+        fib[1] = 1;
+         
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
         }
     }
 }
